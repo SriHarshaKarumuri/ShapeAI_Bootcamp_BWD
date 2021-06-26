450106f45036c01e4a8cdcbed938198a0b73026e
SriHarshaKarumuri <86522232+SriHarshaKarumuri@users.noreply.github.com>
2021-06-26 18:44:08 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>My BootCamp Project</h1>
+      <p>A basic web page by Allmight</p>
+    </div>
+  );
+}
+export default Info;
